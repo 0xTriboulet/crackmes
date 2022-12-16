294d3695ae02b41ac46ccfadcf45d632ebb11051
Steve S <santos.s.salinas@gmail.com>
2022-12-16 15:52:17 -0800

    Add files via upload

---
+++ 0xtriboulet/Happier_Reversing/Happier_Reversing/Happier_Reversing.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string string_xor(string x, string y) {
+    string out_string;
+
+    for (int i = 0; i < x.length(); i++) {
+        out_string += static_cast<char> (x.at(i) ^ y.at(i % y.length()));
+    }
+
+    return out_string;
+
+}
+
+string generate_key() {
+    string keys = "{U[CKi_EoV_E^ToDXUoCUS_^To[UI{'";
+    string static_key = "00000000000000000000000000000ZZ";
+    string out_key = string_xor(static_key, keys);
+    return out_key;
+}
+
+int main()
+{
+    string inputString;
+    std::cout << "Welcome to 0xTriboulet's Reversing Challenge #2!\n\n";
+    std::cout << "Enter the secret key:";
+    std::cin >> inputString;
+
+    if (inputString.compare(generate_key())) {
+        std::cout << "Your key was incorrect! Try again later :(";
+    }
+    else {
+        std::cout << "Your key was correct!";
+    }
+    return 0;
+}
